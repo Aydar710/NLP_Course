97e9934dbbd75e307700fddfa4769ec992285d44
Aydar <aydar710@gmail.com>
2020-04-08 14:54:23 +0300

    corrects

--- NeuralNetworksBasics/Network_2.py
+++ NeuralNetworksBasics/Network_2.py
@@ -7,8 +7,6 @@ from sklearn.metrics import classification_report
 
 from Utils import deserialize_my_normalized_reviews, deserialize_normalized_reviews
 
-tfidf = TfidfVectorizer(max_features=500)
-
 reviews_to_train = deserialize_normalized_reviews()
 
 all_reviews = pandas.read_csv('normallized_reviews.csv', delimiter=',')
@@ -26,13 +24,14 @@ for review in my_reviews:
     reviews_testing.append(" ".join(review))
 
 # tf-idf
+tfidf = TfidfVectorizer(max_features=500)
 tfidf.fit_transform(reviews_training)
-Train_X_Tfidf = tfidf.fit_transform(reviews_training)
-Test_X_Tfidf = tfidf.fit_transform(reviews_testing)
+x_train_tfidf = tfidf.fit_transform(reviews_training)
+x_test_tfidf = tfidf.fit_transform(reviews_testing)
 
 # building model
-Train_Y_keras = keras.utils.to_categorical(all_reviews["label"], 3)
-Test_Y_keras = keras.utils.to_categorical(y_test, 3)
+y_train_categorical = keras.utils.to_categorical(all_reviews["label"], 3)
+y_test_categorical = keras.utils.to_categorical(y_test, 3)
 
 model = Sequential()
 model.add(Dense(512, input_shape=(500,)))
@@ -40,8 +39,8 @@ model.add(Dropout(0.5))
 model.add(Dense(3))
 model.compile(metrics=["accuracy"], optimizer='adam', loss='categorical_crossentropy')
 
-model.fit(Train_X_Tfidf, Train_Y_keras, epochs=1, batch_size=32)
+model.fit(x_train_tfidf, y_train_categorical, epochs=1, batch_size=32)
 
-result = model.predict(Test_X_Tfidf)
+result = model.predict(x_test_tfidf)
 
-print(classification_report(Test_Y_keras.argmax(axis=1), result.argmax(axis=1)))
+print(classification_report(y_test_categorical.argmax(axis=1), result.argmax(axis=1)))
