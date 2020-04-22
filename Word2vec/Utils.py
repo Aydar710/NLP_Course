672e99a08367e42e87c4374492bd4e4c0a63dba7
Aydar <aydar710@gmail.com>
2020-04-22 12:48:08 +0300

    add Word2vec

---
+++ Word2vec/Utils.py
@@ -0,0 +1,54 @@
+import pickle
+import numpy as np
+
+
+def deserialize_normalized_reviews():
+    pickle_in = open("normallized_list.pickle", "rb")
+    normalized_reviews = pickle.load(pickle_in)
+    return normalized_reviews
+
+
+def deserialize_my_normalized_reviews():
+    pickle_in = open("my_reviews_normalized.pickle", "rb")
+    normalized_reviews = pickle.load(pickle_in)
+    return normalized_reviews
+
+
+def serialize_model(model):
+    pickle_out = open("Word2vecModel.pickle", "wb")
+    pickle.dump(model, pickle_out)
+    pickle_out.close()
+
+
+def deserialize_model():
+    pickle_in = open("Word2vecModel.pickle", "rb")
+    model = pickle.load(pickle_in)
+    return model
+
+
+def deserialize_my_normalized_reviews():
+    pickle_in = open("my_reviews_normalized.pickle", "rb")
+    normalized_reviews = pickle.load(pickle_in)
+    return normalized_reviews
+
+
+def get_average_feature_vecs(reviews, model, num_features):
+    count = 0
+    feature_vecs = np.zeros((len(reviews), num_features), dtype='float32')
+    for review in reviews:
+        feature_vecs[count] = get_feature_vecs(review, model, num_features)
+        count = count + 1
+    return feature_vecs
+
+
+def get_feature_vecs(words, model, features_num):
+    feature_vec = np.zeros((features_num,), dtype="float32")
+    word_count = 0
+    index2word_set = set(model.wv.index2word)
+    for word in words:
+        if word in index2word_set:
+            word_count = word_count + 1
+            feature_vec = np.add(feature_vec, model[word])
+
+    feature_vec = np.divide(feature_vec, word_count)
+    return feature_vec
---
+++ Word2vec/Word2vec.py
@@ -0,0 +1,56 @@
+# Reviews for training model, received from pickle
+import gensim.models
+import pandas
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_recall_fscore_support
+
+from Utils import deserialize_normalized_reviews, serialize_model, deserialize_model, get_average_feature_vecs, \
+    deserialize_my_normalized_reviews
+
+reviews_to_train = deserialize_normalized_reviews()
+all_reviews = pandas.read_csv('normallized_reviews.csv', delimiter=',')
+
+# Creating word2vec model
+model = gensim.models.Word2Vec(reviews_to_train, min_count=2, iter=200)
+
+# Serializing model
+# serialize_model(model)
+# model = deserialize_model()
+
+# Printing synonyms
+print(model.wv.most_similar(positive=['разум'], topn=3))
+print(model.wv.most_similar(positive=['творчество'], topn=3))
+print(model.wv.most_similar(positive=['доброта'], topn=3))
+print(model.wv.most_similar(positive=['чувство'], topn=3))
+print(model.wv.most_similar(positive=['мрак'], topn=3))
+
+# get testing reviews
+reviews_to_test = deserialize_my_normalized_reviews()
+test_labels = pandas.read_csv('my_labels.csv', delimiter=',')
+
+# Getting train and test vecs
+train_vecs = get_average_feature_vecs(reviews_to_train, model, 100)
+test_vecs = get_average_feature_vecs(reviews_to_test, model, 100)
+
+# Training model using RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=100, max_depth=20, random_state=0)
+classifier.fit(train_vecs, all_reviews['label'])
+
+# Get predictions on test_vecs
+predictions = classifier.predict(test_vecs)
+
+# Calculating accuracy
+correct_prediction_count = 0
+i = 0
+for prediction in predictions:
+    if prediction == all_reviews.values[i][3]:
+        correct_prediction_count += 1
+    i += 1
+
+accuracy = correct_prediction_count / len(predictions)
+metrics = precision_recall_fscore_support(test_labels, predictions)
+
+print("Accuracy: {}".format(accuracy))
+print(f"Precision(-1, 0, 1) = {metrics[0]}")
+print(f"Recall(-1, 0, 1) = {metrics[1]}")
+print(f"F-Score(-1, 0, 1) = {metrics[2]}")
